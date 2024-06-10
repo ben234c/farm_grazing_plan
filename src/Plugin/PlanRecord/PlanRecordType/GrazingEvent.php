2dd574d0a8804df920bfce302f74c3e418a178f0
Michael Stenta <mike@mstenta.net>
2024-06-10 14:57:53 -0400

    Add a grazing_event plan_record type w/ log reference, start, duration, and recovery.

---
+++ src/Plugin/PlanRecord/PlanRecordType/GrazingEvent.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\farm_grazing_plan\Plugin\PlanRecord\PlanRecordType;
+
+use Drupal\farm_entity\Plugin\PlanRecord\PlanRecordType\FarmPlanRecordType;
+
+/**
+ * Provides the grazing event plan record type.
+ *
+ * @PlanRecordType(
+ *   id = "grazing_event",
+ *   label = @Translation("Grazing event"),
+ * )
+ */
+class GrazingEvent extends FarmPlanRecordType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'log' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Log'),
+        'description' => $this->t('Associates the grazing plan with a movement log.'),
+        'target_type' => 'log',
+        'cardinality' => 1,
+        'required' => TRUE,
+      ],
+      'start' => [
+        'type' => 'timestamp',
+        'label' => $this->t('Planned start date/time'),
+        'required' => TRUE,
+      ],
+      'duration' => [
+        'type' => 'integer',
+        'label' => $this->t('Duration (hours)'),
+        'min' => 1,
+        'max' => 8760,
+        'required' => TRUE,
+      ],
+      'recovery' => [
+        'type' => 'integer',
+        'label' => $this->t('Recovery (days)'),
+        'min' => 1,
+        'max' => 365,
+        'required' => TRUE,
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
+}
