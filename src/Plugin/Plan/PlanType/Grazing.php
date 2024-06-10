b3b3e656e15ab6abeb877bf233278888bf962f00
Michael Stenta <mike@mstenta.net>
2024-06-10 14:22:32 -0400

    Add Grazing plan type.

---
+++ src/Plugin/Plan/PlanType/Grazing.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_grazing_plan\Plugin\Plan\PlanType;
+
+use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
+
+/**
+ * Provides the grazing plan type.
+ *
+ * @PlanType(
+ *   id = "grazing",
+ *   label = @Translation("Grazing"),
+ * )
+ */
+class Grazing extends FarmPlanType {
+
+}
