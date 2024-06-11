f7a3e7ccc35dfc03209f7f6dbc078090ec44b3db
Michael Stenta <mike@mstenta.net>
2024-06-11 15:32:21 -0400

    Add a form for adding grazing events, leveraging the Movement quick form if available.

---
+++ src/Form/GrazingPlanAddEventForm.php
@@ -0,0 +1,262 @@
+<?php
+
+namespace Drupal\farm_grazing_plan\Form;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\log\Entity\LogInterface;
+use Drupal\plan\Entity\PlanInterface;
+use Drupal\plan\Entity\PlanRecord;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Grazing plan add event form.
+ */
+class GrazingPlanAddEventForm extends FormBase {
+
+  /**
+   * Entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The current Request object.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  protected Request $request;
+
+  /**
+   * GrazingPlanAddEventForm constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current Request object.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, Request $request) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->request = $request;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('request_stack')->getCurrentRequest(),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'farm_grazing_plan_add_event_form';
+  }
+
+  /**
+   * Title callback.
+   *
+   * @param \Drupal\plan\Entity\PlanInterface|null $plan
+   *   The plan entity.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *   Returns the title.
+   */
+  public function title(PlanInterface $plan = NULL) {
+    if (empty($plan)) {
+      return $this->t('Add grazing event');
+    }
+    return $this->t('Add grazing event to @plan', ['@plan' => $plan->label()]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, PlanInterface $plan = NULL) {
+    if (empty($plan)) {
+      return $form;
+    }
+    $form_state->set('plan_id', $plan->id());
+
+    $form['log'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Movement log'),
+      '#target_type' => 'log',
+      '#selection_settings' => [
+        'target_bundles' => ['activity'],
+      ],
+      '#ajax' => [
+        'wrapper' => 'grazing-event-details',
+        'callback' => [$this, 'grazingEventDetailsCallback'],
+        'event' => 'autocompleteclose change',
+      ],
+      '#required' => TRUE,
+    ];
+
+    // If a log ID was provided via query parameter, load it and set the
+    // form value.
+    $log_id = $this->request->get('log');
+    if ($log_id) {
+      $log = $this->entityTypeManager->getStorage('log')->load($log_id);
+      if (!empty($log) && $log->bundle() == 'activity') {
+        $form['log']['#default_value'] = $log;
+        $form_state->setValue('log', $log_id);
+      }
+    }
+
+    $form['details'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'id' => 'grazing-event-details',
+      ],
+    ];
+
+    // If the form is being built with a log selected, reset grazing event
+    // details and populate their default values.
+    $log = NULL;
+    if ($form_state->getValue('log')) {
+      $this->resetGrazingEventDetails($form_state);
+      $log = $this->entityTypeManager->getStorage('log')->load($form_state->getValue('log'));
+    }
+    $default_values = $this->grazingEventDefaultValues($log);
+
+    $form['details']['start'] = [
+      '#type' => 'datetime',
+      '#title' => $this->t('Planned start date/time'),
+      '#default_value' => $default_values['start'],
+      '#required' => TRUE,
+    ];
+
+    $form['details']['duration'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Duration (hours)'),
+      '#step' => 1,
+      '#min' => 1,
+      '#max' => 8760,
+      '#default_value' => $default_values['duration'],
+      '#required' => TRUE,
+    ];
+
+    $form['details']['recovery'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Recovery (days)'),
+      '#step' => 1,
+      '#min' => 1,
+      '#max' => 365,
+      '#default_value' => $default_values['recovery'],
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Ajax callback for grazing event details.
+   */
+  public function grazingEventDetailsCallback(array $form, FormStateInterface $form_state) {
+    return $form['details'];
+  }
+
+  /**
+   * Reset grazing event details.
+   */
+  public function resetGrazingEventDetails(FormStateInterface $form_state) {
+    $details_fields = [
+      'start',
+      'duration',
+      'recovery',
+    ];
+    $user_input = $form_state->getUserInput();
+    foreach ($details_fields as $field_name) {
+      unset($user_input[$field_name]);
+    }
+    $form_state->setUserInput($user_input);
+    $form_state->setRebuild();
+  }
+
+  /**
+   * Get default values for grazing event details.
+   *
+   * @param \Drupal\log\Entity\LogInterface|null $log
+   *   A movement log (optional).
+   *
+   * @return array
+   *   Returns a keyed array of grazing event default values, including:
+   *   - start
+   *   - duration
+   *   - recovery
+   */
+  public function grazingEventDefaultValues($log = NULL) {
+
+    // Start with defaults.
+    $values = [
+      'start' => new DrupalDateTime('midnight', $this->currentUser()->getTimeZone()),
+      'duration' => NULL,
+      'recovery' => NULL,
+    ];
+
+    // If a log was provided, load the start date from it.
+    if (!is_null($log) && $log instanceof LogInterface) {
+      $values['start'] = DrupalDateTime::createFromTimestamp($log->get('timestamp')->value);
+    }
+
+    return $values;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    // Require log.
+    $log = $form_state->getValue('log');
+    if (empty($log)) {
+      $form_state->setErrorByName('log', $this->t('Select a movement log.'));
+      return;
+    }
+
+    // Check for existing grazing_event records for the plan and log.
+    $plan_id = $form_state->get('plan_id');
+    $existing = $this->entityTypeManager->getStorage('plan_record')->getQuery()
+      ->accessCheck(FALSE)
+      ->condition('plan', $plan_id)
+      ->condition('log', $log)
+      ->count()
+      ->execute();
+    if ($existing > 0) {
+      $form_state->setErrorByName('log', $this->t('This log is already part of the plan.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $plan_id = $form_state->get('plan_id');
+    $log = $form_state->getValue('log');
+    $record = PlanRecord::create([
+      'type' => 'grazing_event',
+      'plan' => $plan_id,
+      'log' => $log,
+      'start' => $form_state->getValue('start')->getTimestamp(),
+      'duration' => $form_state->getValue('duration'),
+      'recovery' => $form_state->getValue('recovery'),
+    ]);
+    $record->save();
+    $this->messenger()->addMessage($this->t('Added @grazing_event', ['@grazing_event' => $record->label()]));
+    $form_state->setRedirect('entity.plan.canonical', ['plan' => $plan_id]);
+  }
+
+}
