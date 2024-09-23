946923286df56c7a0be0324e6918b93e1bced855
Michael Stenta <mike@mstenta.net>
2024-09-23 11:45:43 -0400

    Override grazing_event entity labels.

--- src/Bundle/GrazingEvent.php
+++ src/Bundle/GrazingEvent.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\farm_grazing_plan\Bundle;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\log\Entity\LogInterface;
 use Drupal\plan\Entity\PlanRecord;
 
@@ -9,6 +10,27 @@ use Drupal\plan\Entity\PlanRecord;
  * Bundle logic for Grazing Event.
  */
 class GrazingEvent extends PlanRecord implements GrazingEventInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function label() {
+
+    // Build label with the referenced plan and log.
+    if ($plan = $this->getPlan()) {
+      if ($log = $this->getLog()) {
+        return $this->t('Grazing event: %log - %plan', ['%log' => $log->label(), '%plan' => $plan->label()]);
+      }
+
+      // Use the plan if no plant reference.
+      return $this->t('Grazing event - %plan', ['%plan' => $plan->label()]);
+    }
+
+    // Fallback to default.
+    return parent::label();
+  }
 
   /**
    * {@inheritdoc}
